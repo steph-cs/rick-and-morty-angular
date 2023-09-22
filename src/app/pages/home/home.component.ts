be0f16622fbd89c42f24132ef24009d494bf5b43
steph_cs <stecampelo@gmail.com>
2023-09-21 22:34:35 -0300

    feat: home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import homeContent from '../../../assets/data/homeContent.json'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+    public homeContent = homeContent
 }
---
+++ src/app/shared/components/text-section/text-section.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-text-section',
+  templateUrl: './text-section.component.html',
+  styleUrls: ['./text-section.component.css']
+})
+export class TextSectionComponent {
+
+    @Input() title: string = "";
+    @Input() content: string = ""
+
+}
